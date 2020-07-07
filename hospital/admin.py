a484a8f25d92c52a5fc0be93a9bbcfe80a4baea4
Rituraj <46903942+Rituraj9@users.noreply.github.com>
2020-07-07 16:36:36 +0530

    Add files via upload

---
+++ hospital/admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from .models import *
+# Register your models here.
+class DoctorAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Doctor, DoctorAdmin)
+
+class PatientAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Patient, PatientAdmin)
+
+class AppointmentAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Appointment, AppointmentAdmin)
+
+class PatientDischargeDetailsAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(PatientDischargeDetails, PatientDischargeDetailsAdmin)
+
+admin.site.register(Contact)
+admin.site.register(Department)
